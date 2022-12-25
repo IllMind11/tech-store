49ee45b98353f61b5e82041f9a42e8b36d34b7f8
khusnazarovakbar@gmail.com <khushnazarovislom@gmail.com>
2022-12-25 21:40:16 +0500

    added tailwind-plugin

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="w-full bg-slate-500 flex justify-center items-center">
-      <h1 className="p-8 text-slate-200 text-xl">Hello Boi</h1>
+      <h1 className="p-8 text-slate-100 text-3xl">Hello Boi</h1>
     </div>
   );
 }
