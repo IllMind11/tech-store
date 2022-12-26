4f2b4aaf8552597346fd0da7dd78b1c0842c262d
Akbar1437 <khusnararov@gmail.com>
2022-12-26 17:26:12 +0500

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="w-full bg-slate-500 flex justify-center items-center">
       <h1 className="p-8 text-slate-100 text-3xl">Hello Boi</h1>
+      <h2 className="p-8 text-slate-100 text-3xl">Hi</h2>
     </div>
   );
 }
